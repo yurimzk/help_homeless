bdb3ec92c3ef7556497808d1078b375c38d74880
Leonardo Alves <leonardo.rba@uol.com.br>
2023-06-08 16:24:03 -0300

    comments done

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import CommentsController from "./comments_controller"
+application.register("comments", CommentsController)
