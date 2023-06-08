2758a68771e78106d50c26865e5e96c50aba6e8e
Pedro Zoffoli <pedrozoffoli@gmail.com>
2023-06-08 16:24:45 -0300

    1- added form for join, 2- added some front, 3- added helpers, 4- changes controller events/participant

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -31,6 +31,8 @@ class EventsController < ApplicationController
   def show
     @posts = Post.where(event_id: @event.id)
     @post = Post.new
+    @participants = Participant.where(event_id: @event.id)
+    @participant = Participant.new(user_id: current_user, event_id: params[:id])
   end
 
   def edit
